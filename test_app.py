856e7b5ece3011affaa158c909300a0dbc3bbe79
Aditya <mishimakazuya3218@gmail.com>
2025-05-26 18:51:14 +0530

    Fix my Flask app to listen

--- test_app.py
+++ test_app.py
@@ -1,6 +1,9 @@
 import pytest
-from app import app
-
+from app import appdef test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Cloud DevOps Automation" in response.data
+    assert response.content_type == 'text/html; charset=utf-8'
 @pytest.fixture
 def client():
     with app.test_client() as client:
